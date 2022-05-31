c7b6ed28c44a89a0d2971701e51da774e2af894a
Eliana Henderson <elianalhenderson@gmail.com>
2022-05-30 17:14:11 -0700

    React State

---
+++ src/components/HelloView/HelloView.stories.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HelloView } from "./HelloView";
+
+export default {
+  title: "Bonus Components/HelloView",
+  component: HelloView,
+};
+
+const Template = () => <HelloView />;
+export const Default = Template.bind({});
