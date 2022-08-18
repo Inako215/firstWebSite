7a5dc64df558309121e503873fdce54bc4b7a967
Eliana Henderson <Elianalhenderson@gmail.com>
2022-08-18 01:27:56 +0000

    Added PilotUpdateView.stories.jsx

---
+++ src/components/PilotUpdateView/PilotUpdateView.stories.jsx
@@ -0,0 +1,10 @@
+import { PilotUpdateView } from "./PilotUpdateView";
+
+export default {
+  title: "Components/PilotUpdateView",
+  component: PilotUpdateView,
+};
+
+const Template = () => <PilotUpdateView />;
+
+export const Default = Template.bind({});
